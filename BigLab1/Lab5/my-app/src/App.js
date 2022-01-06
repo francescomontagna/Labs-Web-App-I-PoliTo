aca147f77af2aec5b63221ad751fb47f613d9434
Francesco Montagna <38191733+LilMowgli@users.noreply.github.com>
2022-01-06 22:44:19 +0100

    Added all compontents. TODO: fix 2 filters and font

--- BigLab1/Lab5/my-app/src/App.js
+++ BigLab1/Lab5/my-app/src/App.js
@@ -1,129 +1,34 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col'; 
-import Form from 'react-bootstrap/Form';
-import ListGroup from 'react-bootstrap/ListGroup';
 
 import dayjs from 'dayjs';
 
-// Fontawesome: see https://fontawesome.com/v5.15/how-to-use/on-the-web/using-with/react
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUserCircle, faUser} from '@fortawesome/free-solid-svg-icons';
-import { faCheckSquare} from '@fortawesome/free-regular-svg-icons';
-
-import { ManagerNavbar, TaskList} from './ManagerComponents';
+import { ManagerNavbar, MainComponent} from './ManagerComponents';
 import { Task } from './Task';
 
-library.add(faUserCircle, faUser, faCheckSquare);
 
 const tasks = [
   new Task(1, 'Complete Lab 4', 1, 1, dayjs('01/25/2022').format('DD MMM YYYY')),
-  new Task(2, 'Buy some groceries', 0, 0, dayjs('01/10/2022').format('DD MMM YYYY')),
+  new Task(2, 'Buy some groceries', 0, 0, dayjs('01/06/2022').format('DD MMM YYYY')),
   new Task(3, 'Read a good book!', 0, 0, dayjs('02/25/2022').format('DD MMM YYYY')),
   new Task(4, 'Watch Mr. Robot', 0, 1, undefined)
 ]
 
 
-
-// This is a React Component
 function App() {
 
-  // Returns a React Element. JSX syntax
   return (
     <div className="App">
       
       <ManagerNavbar />
 
       <Container fluid>
-
-        <Row className='vheight-100'>
-          <Col as='aside' className='pt-2' md={3}>
-            <ListGroup variant="flush" defaultActiveKey="#all">
-              <ListGroup.Item action href="#all" className="text-secondary">All</ListGroup.Item>
-              <ListGroup.Item action href="#important" className="text-secondary">Important</ListGroup.Item>
-              <ListGroup.Item action href="#today" className="text-secondary">Today</ListGroup.Item>
-              <ListGroup.Item action href="#next-week" className="text-secondary">Next 7 Days</ListGroup.Item>
-              <ListGroup.Item action href="#private" className="text-secondary">Private</ListGroup.Item>
-            </ListGroup>
-          </Col>
-
-          <Col as='main' sm={9}>
-            <h1 className="pt-1 filter-heading d-flex justify-content-left">All</h1>
-
-            <TaskList tasks={tasks}/>
-
-              {/* <ListGroup as="ul" variant="flush">
-
-                <ListGroup.Item as={Row} className="gx-0 mx-0 d-flex todo-item important">
-                  <Form.Group controlId="1" as={Col} xs={6} className="gx-0">
-                    <Form.Check type="checkbox" label="Complete Lab 4"/>
-                  </Form.Group>
-
-                  <Col as="span" xs={1}>
-                    <FontAwesomeIcon icon={faUser} />
-                  </Col>
-
-                  <Col as="span" xs={5} className="date">
-                    {dayjs('2022-01-25').format('DD MMM YYYY')}
-                  </Col>
-                </ListGroup.Item>
-
-
-
-                <ListGroup.Item as={Row} className="gx-0 mx-0 d-flex todo-item">
-                  <Form.Group controlId="2" as={Col} xs={6}>
-                    <Form.Check type="checkbox" label="Buy some groceries" />
-                  </Form.Group>
-
-                  <Col as="span" xs={1}>
-                  </Col>
-
-                  <Col as="span" xs={5} className="date">
-                  {dayjs('2022-01-10').format('DD MMM YYYY')}
-                  </Col>
-                </ListGroup.Item>
-
-                <ListGroup.Item as={Row} className="gx-0 mx-0 d-flex todo-item">
-                  <Form.Group controlId="1" as={Col} xs={6}>
-                    <Form.Check type="checkbox" label="Read a good book!" />
-                  </Form.Group>
-
-                  <Col as="span" xs={1}>
-                  </Col>
-
-                  <Col as="span" xs={5} className="date">
-                  {dayjs('2022-02-25').format('DD MMM YYYY')}
-                  </Col>
-                </ListGroup.Item>
-
-                <ListGroup.Item as={Row} className="gx-0 mx-0 d-flex todo-item">
-                  <Form.Group controlId="1" as={Col} xs={6}>
-                    <Form.Check type="checkbox" label="Watch Mr. Robot" />
-                  </Form.Group>
-
-                  <Col as="span" xs={1}>
-                    <FontAwesomeIcon icon={faUser} />
-                  </Col>
-
-                  <Col as="span" xs={5} className="date">
-                    
-                  </Col>
-                </ListGroup.Item>
-
-              </ListGroup> */}
-
-          </Col>
-
-        </Row>
-
+        <MainComponent tasks={tasks}/>
       </Container>
 
     </div>
   );
 }
 
-// Needed to make it importable (index.js)
 export default App;
--- BigLab1/Lab5/my-app/src/ManagerComponents.js
+++ BigLab1/Lab5/my-app/src/ManagerComponents.js
@@ -4,11 +4,12 @@ import Form from 'react-bootstrap/Form';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'; 
-import useState from 'react';
+import { useState } from 'react';
 
+// Fontawesome: see https://fontawesome.com/v5.15/how-to-use/on-the-web/using-with/react
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle} from '@fortawesome/free-solid-svg-icons';
-import { faCheckSquare, faUser } from '@fortawesome/free-regular-svg-icons';
+import { faUserCircle, faUser} from '@fortawesome/free-solid-svg-icons';
+import { faCheckSquare } from '@fortawesome/free-regular-svg-icons';
 
 import dayjs from 'dayjs';
 
@@ -33,29 +34,74 @@ function ManagerNavbar(props) {
     );
 }
 
+
+function MainComponent(props) {
+    const [tasks, setTasks] = useState(props.tasks);
+    const [header, setHeader] = useState("All");
+
+    return(
+        <Row className='vheight-100'>
+            <Col as='aside' className='pt-2' md={3}>
+            <ManagerFilters tasks={props.tasks} setTasks={setTasks} header={header} setHeader={setHeader} />
+            </Col>
+
+            <Col as='main' sm={9}>
+                <h1 className="pt-1 filter-heading d-flex justify-content-left">{header}</h1>
+                <TaskList tasks={tasks}/>
+            </Col>
+        </Row>
+    );
+}
+
 // Needed in order to keep track of the active state
 function ManagerFilters(props) {
 
-    const [tasks, setTasks] = useState(props.tasks);
-
     // Implement all the task filters
     const allFilter = () => {
-        return setTasks(props.tasks);
+        props.setTasks(props.tasks);
+        props.setHeader("All");
     }
     
     const importantFilter = () => {
-         
+         props.setTasks(() => props.tasks.filter(t => t.isUrgent));
+         props.setHeader("Important");
+    }
+
+    const privateFilter = () => {
+        props.setTasks(() => props.tasks.filter(t => t.isPrivate));
+        props.setHeader("Private");
+    }
+
+    const todayFilter = () => {
+        // TODO Fix
+        const now = dayjs();
+        props.setTasks(
+            props.tasks.filter(
+                task => task.hasDeadline ? task.deadline.date() === now.date() && task.deadline.month() === now.month() && task.deadline.year() === now.year() : false
+            )
+        );
+
+        props.setHeader("Today");
+    }
+
+    const nextWeekFilter = () => {
+
     }
 
     return (
-        undefined
+        <ListGroup variant="flush" defaultActiveKey="#all">
+            <ListGroup.Item action href="#all" className="text-secondary" onClick={() => allFilter()}>All</ListGroup.Item>
+            <ListGroup.Item action href="#important" className="text-secondary" onClick={() => importantFilter()}>Important</ListGroup.Item>
+            <ListGroup.Item action href="#today" className="text-secondary" onClick={() => todayFilter()}>Today</ListGroup.Item>
+            <ListGroup.Item action href="#next-week" className="text-secondary" onClick={() => nextWeekFilter()}>Next 7 Days</ListGroup.Item>
+            <ListGroup.Item action href="#private" className="text-secondary" onClick={() => privateFilter()}>Private</ListGroup.Item>
+        </ListGroup>
     );
 }
 
 
 // Needed to interact with filters
 function TaskList(props) {
-    // pass the task as property
     return (<ListGroup as="ul" variant="flush">
         {props.tasks.map( t => <ReactTask key={t.id} description={t.description} urgent={t.isUrgent} private={t.isPrivate} deadline={t.deadline}/>)}
     </ListGroup>);
@@ -64,7 +110,7 @@ function TaskList(props) {
 function ReactTask(props){
     let groupItemClass = "gx-0 mx-0 d-flex todo-item";
 
-    if (props.isUrgent)
+    if (props.urgent)
         groupItemClass += " important";
 
     return (
@@ -74,7 +120,7 @@ function ReactTask(props){
             </Form.Group>
 
             <Col as="span" xs={1}>
-                {props.isPrivate && <FontAwesomeIcon icon={faUser} /> /* Conditionally render an element*/} 
+                {props.private && <FontAwesomeIcon icon={faUser} /> /* Conditionally render an element*/} 
             </Col>
 
             <Col as="span" xs={5} className="date">
@@ -84,4 +130,4 @@ function ReactTask(props){
     );
 }
 
-export {ManagerNavbar, TaskList};
+export {ManagerNavbar, MainComponent};
